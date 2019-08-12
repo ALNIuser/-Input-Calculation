7bec04d02592be3c3119af8bdfaab733f5ec9f67
Aleksandr <48583343+ALNIuser@users.noreply.github.com>
2019-08-12 17:47:44 +0300

    Update apple.py

--- apple.py
+++ apple.py
@@ -1,10 +1,11 @@
 number_input = input('Введите количество яблок ')
 school_child = input('Введите количество учеников ')
-print('Расчет равного количества яблок на каждого ученика')
-print('Яблок в корзине', number_input)
+#print('Расчет равного количества яблок на каждого ученика')
+print('Яблок в корзине было', number_input)
 print('Школьников', school_child, 'человек')
 number_apple = int(number_input) // int(school_child)
-if int(number_apple) < int(school_child):
-    print('Количество яблок меньше количества учеников')
-print("Каждый ученик получит по", number_apple, 'яблоку')
+if int(number_input) < int(school_child):
+    print('Количество яблок меньше количества учеников, поэтому яблоки раздаваться не будут')
+else:
+    print("Каждый ученик получит по", number_apple, 'яблоку')
 print('В корзине останется ', int(number_input) % int(school_child))
