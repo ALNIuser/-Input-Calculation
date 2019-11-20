142eb42f76e1cd3847ab35eb571e112245c9a1a5
ALNI <alni14@ya.ru>
2019-11-20 22:40:19 +0300

    जोड़ा डैश

--- apple.py
+++ apple.py
@@ -1,14 +1,14 @@
 number_input = input('Введите количество яблок : ')
 school_child = input('Введите количество учеников : ')
 print('Расчет равного количества яблок на каждого ученика :')
-print('Яблок в корзине было', number_input)
-print('Школьников', school_child, 'человек')
+print('- Яблок в корзине было', number_input)
+print('- Школьников', school_child, 'человек')
 number_apple = int(number_input) // int(school_child)
 if int(number_input) < int(school_child):
-    print('Количество яблок меньше количества учеников, поэтому яблоки раздаваться не будут')
+    print('- Количество яблок меньше количества учеников, поэтому яблоки раздаваться не будут')
 else:
-    print("Каждый ученик получит по", number_apple, 'яблок')
-print('В корзине останется ', int(number_input) % int(school_child) , 'яблок')
+    print("- Каждый ученик получит по", number_apple, 'яблок')
+print('- В корзине останется ', int(number_input) % int(school_child) , 'яблок')
 
 
 
