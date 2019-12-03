97a8d55be5b4f13d5f2c84fc51b1fcc6e00e54bb
Aleksandr <48583343+ALNIuser@users.noreply.github.com>
2019-12-03 11:37:46 +0300

    Update translate

--- apple.py
+++ apple.py
@@ -1,7 +1,7 @@
 number_input = input('Enter the number of apples : ')
 school_child = input('Enter the number of students : ')
 print('Calculation of an equal number of apples per student :')
-print('- Яблок в корзине было', number_input)
+print('- The apple in the basket was ', number_input)
 print('- Schoolchildren ', school_child, 'человек')
 number_apple = int(number_input) // int(school_child)
 if int(number_input) < int(school_child):
