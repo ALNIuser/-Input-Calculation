f6eefa82176817bcb798c63bd50363cb1915b918
Aleksandr <48583343+ALNIuser@users.noreply.github.com>
2019-12-05 11:16:38 +0300

    Update space

--- apple.py
+++ apple.py
@@ -5,8 +5,7 @@ print('- The apple in the basket was ', number_input)
 print('- Schoolchildren ', school_child, 'человек')
 number_apple = int(number_input) // int(school_child)
 if int(number_input) < int(school_child):
-    print('- 
-The number of apples is less than the number of students, therefore, apples will not be distributed ! ')
+    print('-The number of apples is less than the number of students, therefore, apples will not be distributed !')
 else:
     print("- Каждый ученик получит по", number_apple, 'яблок')
 print('- В корзине останется ', int(number_input) % int(school_child) , 'яблок')
