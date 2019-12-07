8933b723ff8929c41ef6d81574749dbdb4e43815
Aleksandr <48583343+ALNIuser@users.noreply.github.com>
2019-12-07 23:59:18 +0300

    Update apple.py

--- apple.py
+++ apple.py
@@ -2,7 +2,7 @@ number_input = input('Enter the number of apples : ')
 school_child = input('Enter the number of students : ')
 print('Calculation of an equal number of apples per student :')
 print('- The apple in the basket was ', number_input)
-print('- Schoolchildren ', school_child, 'человек')
+print('- Number of students ', school_child, 'человек')
 number_apple = int(number_input) // int(school_child)
 if int(number_input) < int(school_child):
     print('-The number of apples is less than the number of students, therefore, apples will not be distributed !')
