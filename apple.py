6ad85e673cac5291b02ac5e8c320068f15867594
ALNIuser <alni14@ya.ru>
2021-05-16 16:33:29 +0300

    Update

--- apple.py
+++ apple.py
@@ -5,7 +5,7 @@ print('- Яблок в корзине ', number_input)
 print('- Количество людей ', school_child )
 number_apple = int(number_input) // int(school_child)
 if int(number_input) < int(school_child):
-    print('-The number of apples is less than the number of students, therefore, apples will not be distributed !')
+    print('- Количество яблок меньше, чем количество людей, поэтому яблоки раздавать не будут !')
 else:
     print("- Each student will receive", number_apple, 'apples')
 print('- Will remain in the basket ', int(number_input) % int(school_child) , 'apples')
