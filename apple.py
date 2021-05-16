3938d51f8757c7efcf50ff9cc5a76b9262d33d67
ALNIuser <alni14@ya.ru>
2021-05-16 16:29:00 +0300

    Update translate

--- apple.py
+++ apple.py
@@ -1,8 +1,8 @@
 number_input = input('Введите количество яблок : ')
 school_child = input('Введите количество человек: ')
-print('Calculation of an equal number of apples per student :')
-print('- The apple in the basket was ', number_input)
-print('- Number of students ', school_child, 'человек')
+print(' Расчет равного количества яблок на одного человека:')
+print('- Яблок в корзине ', number_input)
+print('- Количество людей ', school_child, 'человек')
 number_apple = int(number_input) // int(school_child)
 if int(number_input) < int(school_child):
     print('-The number of apples is less than the number of students, therefore, apples will not be distributed !')
