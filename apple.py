e0833362a95dc45225f23edeed3394eb8a11040e
ALNIuser <alni14@ya.ru>
2021-05-19 06:56:50 +0300

    Update

--- apple.py
+++ apple.py
@@ -3,7 +3,7 @@ number_input = input('Введите количество яблок : ')
 school_child = input('Введите количество человек: ')
 print(' Расчет равного количества яблок на одного человека:')
 print('- Яблок в корзине ', number_input)
-print('- Количество людей ', school_child )
+print('- Количество людей ', school_child)
 number_apple = int(number_input) // int(school_child)
 if int(number_input) < int(school_child):
     print('- Количество яблок меньше, чем количество людей, поэтому яблоки раздавать не будут !')
